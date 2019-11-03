19be9a396c1c712bc2e7ecac85260c1d9b1a2e0a
Thomas Cherryhomes <thom.cherryhomes@gmail.com>
2019-11-03 00:05:01 -0500

    fix drive detection routine.

--- src/drive_detect.c
+++ src/drive_detect.c
@@ -2,6 +2,9 @@
  * Routine to detect drive density/sector size
  */
 
+#include <atari.h>
+#include <6502.h>
+#include <stddef.h>
 #include "drive_detect.h"
 #include "sector.h"
 
@@ -20,9 +23,21 @@ extern unsigned char sector_buffer;
 unsigned short drive_detect(unsigned char s, unsigned char d, PercomBlock* pb)
 {
   unsigned char status;
+  struct regs r;
+  
+  OS.dcb.ddevic=0x31; // disk drives
+  OS.dcb.dunit=s;     // unit #
+  OS.dcb.dcomnd='R';  // Read Sector
+  OS.dcb.dbuf=NULL;   // Point to sector buffer
+  OS.dcb.dtimlo=0x0F; // Timeout
+  OS.dcb.dstats=0x00; // tell SIO it's null op
+  OS.dcb.dbyt=0;     // no buffer needed.
+  OS.dcb.daux=4;     // Read first non-boot sector.
 
+  r.pc=0xE459;
+  _sys(&r);
+  
   status=percom_get(s,pb);
-  status=sector_get(s,DETECT_READ_SECTOR,pb->sector_size,&sector_buffer);
 
   if (pb->reserved1==0xFF)
     {
@@ -30,7 +45,7 @@ unsigned short drive_detect(unsigned char s, unsigned char d, PercomBlock* pb)
       // to read sector 1040 to detect a medium
       // density disk.
 
-      /* status=sector_get(s,1040,128,&sector_buffer); */
+      status=sector_get(s,1040,128,&sector_buffer);
 
       if (status==0x01) // read successful.
 	{
